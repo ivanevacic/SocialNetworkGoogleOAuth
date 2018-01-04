d8379b439b5a92957ad7bbfd89a0b398e99ef1a5
ivanevacic <ivanevacicweb@gmail.com>
2018-01-04 13:51:36 +0100

    Started googleoauth feature

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport')
+
+//passport config
+require('./config/passport')(passport);   //immediatelly pass passport
+//Load routes
+const auth = require('./routes/auth');  //load file to use routes
 
 const app = express();
 
 app.get('/', (req, res)=>{
   res.send('It works');
 });
+
+//use routes
+app.use('/auth',auth);
 
 
 const port = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://ivan:ivanevacic@ds239387.mlab.com:39387/socialnetwork-production',
+  googleClientID:'263692086014-05mjud48tnn9se9saumo5ro5gku6k24j.apps.googleusercontent.com',
+  googleClientSecret:'B8CW-rh7YdT2oDR9ejULFKfM'
+}
