98c9d9a9ecfe45f8f138d2af86c538f321b99cf8
ivanevacic <ivanevacicweb@gmail.com>
2018-01-06 10:17:50 +0100

    Added comments functionality-needs work

--- routes/stories.js
+++ routes/stories.js
@@ -103,4 +103,24 @@ router.delete('/:id', (req, res)=>{
     });
 });
 
+
+//add comment
+router.post('/comment/:id', (req, res)=>{
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story =>{
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+    ///add to comments array
+    story.comments.unshift(newComment);
+    story.save()
+      .then(story =>{
+        res.redirect(`/stories/show/${story.id}`);
+      });
+  });
+});
+
 module.exports = router;
